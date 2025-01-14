8f36a926ff298e5f73b9f12aa722e7b5c4ed39dc
Sajeel Ahsan <sajeel.ahsan2298@gmail.com>
2025-01-14 10:48:29 +0100

    all setup are configured

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 //require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import connectDB from "./databse/index.js";
+import { app } from './app.js';
 
 dotenv.config({
-  path: "/.env"
+  path: "./.env"
 });
 
 connectDB()
 .then(()=> {
    app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server is running at post: ${process.env.PORT}`);
+    console.log(`Server is running at port: ${process.env.PORT}`);
    });
 })
 .catch((err)=>{
