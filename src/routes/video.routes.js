30a66e162266f693544bcf8221dd0f6b380774a7
Sajeel Ahsan <sajeel.ahsan2298@gmail.com>
2025-01-22 18:42:18 +0100

    add video routes with JWT verification and video publishing functionality

---
+++ src/routes/video.routes.js
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import {
+    publishAVideo,   
+} from "../controllers/video.controller.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+import {upload} from "../middlewares/multer.middleware.js"
+
+const router = Router();
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+router
+    .route("/")
+    .get()
+    .post(
+        upload.fields([
+            {
+                name: "videoFile",
+                maxCount: 1,
+            },
+            {
+                name: "thumbnail",
+                maxCount: 1,
+            },
+        ]),
+        publishAVideo
+    );
+
+router
+    .route("/:videoId")
+    .get()
+    .delete()
+    .patch(upload.single("thumbnail"), );
+
+router.route("/toggle/publish/:videoId").patch();
+
+export default router
